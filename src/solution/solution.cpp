08bde40e79ace710c9c98d2e0a4f2ea87bf03ec0
Maximilian Starke <maximilian.starke@medionmail.com>
2017-04-29 18:12:13 +0200

    working but not fast

--- src/solution/solution.cpp
+++ src/solution/solution.cpp
@@ -1,6 +1,139 @@
 #include "solution.h"
+#include <iostream>
+
+std::map < std::vector<std::vector<bool>>, unsigned long long > all;
+std::list<std::vector<std::vector<bool>>> cur;
+std::list<std::vector<std::vector<bool>>> nex;
+std::list<std::vector<std::vector<bool>>> pl;
+
+std::list<std::vector<std::vector<bool>>>* pc;
+std::list<std::vector<std::vector<bool>>>* pn;
+
+int GX;
+int GY;
+int mind_zeile{ 0 };
+
+bool new_free(int& xx, int& yy, const Field& field) {
+	for (int x = mind_zeile; x < GX; ++x) {
+		for (int y = 0; y < GY; ++y) {
+			if (field.at(x).at(y) == false) {
+				xx = x;
+				yy = y;
+				mind_zeile = x > 0 ? x - 1 : 0;
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+bool adv_at(Field& field, int x, int y) {
+	static bool dummy;
+	try {
+		return (field.at(x).at(y));
+	}
+	catch (...) {
+		dummy = true;
+		return dummy;
+	}
+}
+
+const std::vector<Field> tiles = {
+	{
+		{0,1,1,1}, {0,0,0,0}, {0,0,0,0}
+	},
+	{
+		{0,1,1,0}, {0,0,1,0}, {0,0,0,0}
+	},
+	{
+		{0,1,1,0}, {0,1,0,0}, {0,0,0,0}
+	},
+	{
+		{0,1,0,0}, {0,1,1,0}, {0,0,0,0}
+	},
+	{
+		{0,1,0,0}, {0,1,0,0}, {0,1,0,0}
+	},
+	{
+		{0,1,0,0}, {1,1,0,0}, {0,0,0,0}
+	}
+};
 
 void solve(int matrix_x, int matrix_y, long long int& result){
+	if (matrix_y > matrix_x) return solve(matrix_y, matrix_x, result);
+
+	GX = matrix_x;
+	GY = matrix_y;
+
+	Field field = std::vector<std::vector<bool>>(matrix_x, std::vector<bool>(matrix_y, false));
+	all[field] = 1;
+	cur.push_back(field);
+
+	pc = &cur;
+	pn = &nex;
+	int counter{ 0 };
+	while (true)
+	{
+		std::cout << "Lege Teil Nr" << ++counter << '\n';
+		//int cinner{ 0 };
+		auto stateiter = pc->begin();
+		while (stateiter != pc->end())
+		{	
+			//++cinner;
+			auto state = *(stateiter++);
+			
+			//std::cout << cinner << "Wir haben ein alt state vom stack genommen\n" << "es gibt noch restliche:" << pc->size() << std::endl;
+			int xn, yn;
+			bool has_free = new_free(xn, yn, state);
+			if (!has_free) {
+				//std::cout << all[state] << "Möglichkeiten";
+				result = all[state];
+				return;
+			}
+			//std::cout << "es hat freie pläütze\n";
+			for (auto tile : tiles) {
+				Field nstate = state;
+				// schauen ob man tile drauflegen kann.
+				bool not_okay{ false };
+				for (int i = 0; i < 3; ++i) {
+					for (int j = 0; j < 4; ++j) {
+						if (tile.at(i).at(j)) {
+							if (adv_at(nstate, xn + i, yn + j - 1)) {
+								not_okay = true;
+							}
+						}
+					}
+				}
+				if (not_okay) continue;
+				for (int i = 0; i < 3; ++i) {
+					for (int j = 0; j < 4; ++j) {
+						if (tile.at(i).at(j)) {
+							nstate.at(xn + i).at(yn + j - 1) = true;
+						}
+					}
+				}
+				// nstate in die map hinzufügen
+				try {
+					all.at(nstate) += all.at(state);
+				}
+				catch (...) {
+					all.operator[](nstate) = all.at(state);
+					pn->push_back(nstate);
+				}
+				// nstate auf die neue liste setzen.
+
+				//std::cout << "jetzt auf pn:" << pn->size() << std::endl;
+			}// for tiles
+		}
+
+
+		/*for (const auto& item : pl) {
+			all.erase(item);
+		}*/
+		pl = *pc;
+
+		std::swap(pn, pc);
+	}
 
 	// insert your code to solve the task
 	result = 0;
--- src/solution/solution.h
+++ src/solution/solution.h
@@ -3,7 +3,20 @@
 */
 #ifndef SOLUTION_H
 #define SOLUTION_H
+#include <vector>
+#include <map>
+#include <list>
+//#include <boost//multiprecision/cpp_int.hpp>
+
+using Field = std::vector<std::vector<bool>>;
+
 
 void solve(int matrix_x, int matrix_y, long long int& result);
+
+
+struct coord {
+	int x;
+	int y;
+};
 
 #endif
